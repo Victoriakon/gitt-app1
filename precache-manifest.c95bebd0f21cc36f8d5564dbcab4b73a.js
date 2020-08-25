ea965fbc016313095bc388cec7ce88cc57825abd
victoria <larikon@hotmail.com>
2020-08-25 19:35:30 +0300

    Updates

---
+++ precache-manifest.c95bebd0f21cc36f8d5564dbcab4b73a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe0c98611fad6f3f5979b93aa97b22a",
+    "url": "/gitt-app1/index.html"
+  },
+  {
+    "revision": "4418ec855c042f8d50eb",
+    "url": "/gitt-app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd1152e77c716a8230d0",
+    "url": "/gitt-app1/static/js/2.939bf6c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitt-app1/static/js/2.939bf6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4418ec855c042f8d50eb",
+    "url": "/gitt-app1/static/js/main.3ea87597.chunk.js"
+  },
+  {
+    "revision": "90b64035868be3693f94",
+    "url": "/gitt-app1/static/js/runtime-main.0bd923f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitt-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
